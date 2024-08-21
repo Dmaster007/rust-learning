bdc753e36193362d1beb264b016d86e5d6be9841
Durgesh Agrawal <durgesh3103@gmail.com>
2024-08-21 14:09:14 +0000

    guess_it game completed

--- rust-learning/guess_it/src/main.rs
+++ rust-learning/guess_it/src/main.rs
@@ -1,12 +1,32 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
     println!("hello welcome to guessing game");
 
-    println!("guess the first number");
-    let mut guess = String::new();
+    let seceret_num: i32 = rand::thread_rng().gen_range(1..=100);
+    // println!("number gen {seceret_num}");
+    loop {
+        println!("guess a number");
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("pls enter a valid input");
+                continue;},
+        };
+        // println!("You guessed: {}", guess_num);
+        match guess.cmp(&seceret_num) {
+            Ordering::Equal => {
+                println!("you won!!!");
+                break;
+            }
+            Ordering::Greater => println!("Too Big try lesser"),
+            Ordering::Less => println!("Too small try bigger"),
+        }
+    }
 }
